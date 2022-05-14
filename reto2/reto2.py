0efa29aba90afbd39ce1f8b6a316dea0f66bfd69
Tulio-Rangel <tjrn20@hotmail.com>
2022-05-14 12:18:26 -0500

    first commit

---
+++ reto2/reto2.py
@@ -0,0 +1,35 @@
+estudiante1 = {
+    'cédula': '1234564567',
+    'nombre': 'Juancho',
+    'nota_fundamentos': 4.5
+}
+estudiante2 = {
+    'cédula': '1034464463',
+    'nombre': 'Pedro',
+    'nota_fundamentos': 4.2
+}
+estudiante3 = {
+    'cédula': '1000564566',
+    'nombre': 'Clara',
+    'nota_fundamentos': 4.5
+}
+estudiante4 = {
+    'cédula': '1300564367',
+    'nombre': 'Mafe',
+    'nota_fundamentos': 4.1
+}
+estudiante5 = {
+    'cédula': '1030561507',
+    'nombre': 'Juancha',
+    'nota_fundamentos': 4.0
+}
+estudiante6 = {
+    'cédula': '1204504462',
+    'nombre': 'Trosky',
+    'nota_fundamentos': 3.9
+}
+estudiante7 = {
+    'cédula': '1054062533',
+    'nombre': 'Diógenes',
+    'nota_fundamentos': 3.8
+}
